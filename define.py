ddd38f08404372bb73fdb5c76ef1bda5104cc7e8
T-WK <ktwqwe12@daum.net>
2023-06-24 01:31:44 +0900

    상수 추가
    - 메인기능 관련 상수 추가

--- define.py
+++ define.py
@@ -1,5 +1,16 @@
+# 실행.txt 로 테스트 할 때 썼음
 SETTING_FILE = '실행.txt'
 COMBINE_MODE = 1
 EXTRACTION_MODE = 2
 SPLIT_STR = '='
+
+# 옵션값
 ENCODING = 'UTF-8'
+VIEW_SIZE = '500x200+400+300'
+ORDER_MODE = 1
+INVOICE_MODE = 2
+
+# 액셀 시트
+NAME = 'name'
+ADDRESS = 'address'
+PHONE = 'phone'
