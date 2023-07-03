9bc5e3c6f025a159d3270a4901482e38565eaf2a
T-WK <ktwqwe12@daum.net>
2023-07-03 23:31:54 +0900

    DFINE 수정
    - 프로그램 버전 상수 추가
    - 택배 column정보를 담은 딕셔너리 추가
    - 주문합치기 모드 상수 추가

--- define.py
+++ define.py
@@ -2,6 +2,7 @@
 SETTING_FILE = '실행.txt'
 COMBINE_MODE = 1
 EXTRACTION_MODE = 2
+MAKE_MODE = 3
 SPLIT_STR = '='
 
 # 옵션값
@@ -9,8 +10,27 @@ ENCODING = 'UTF-8'
 VIEW_SIZE = '500x200+400+300'
 ORDER_MODE = 1
 INVOICE_MODE = 2
+VERSION = 'ver 1.0.0'
 
 # 액셀 시트
 NAME = 'name'
 ADDRESS = 'address'
 PHONE = 'phone'
+
+COLUMN_VALUES = {
+    'NAME':'수하인명',
+    'BLANK':'-',
+    'ADDRESS':'수하인주소',
+    'PHONE1':'수하인 전화번호 1',
+    'PHONE2':'수하인 전화번호 2',
+    'COUNT':'수량',
+    'DELIVERY_COST':'택배운임',
+    'DELIVERY_TYPE':'운임구분',
+    'ITEM':'품목명',
+    'OPTION1':'추가옵션 1',
+    'OPTION2':'추가옵션 2',
+    'DELIVERY_MSG':'배송메세지',
+    'MALL':'쇼핑몰명',
+    'ORDER_NUM':'주문번호',
+    'INVOICE_NUM':'송장번호'
+}
